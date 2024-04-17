932b56b52a23c6a8cefa6138220a1ff62ad6d52f
Vlad <thatvlad@icloud.com>
2024-04-17 16:22:08 +0200

    Add test

--- daisyduino/simple-fix-template/simple-fix-template.ino
+++ daisyduino/simple-fix-template/simple-fix-template.ino
@@ -1,24 +1,22 @@
-// SYNTHUX ACADEMY ////////////////////////////////////////////
-// TEMPLATE PROJECT FOR SIMPLE BOARD AND DAISY SEED ///////////
+// SYNTHUX ACADEMY /////////////////////////////////////////
+// TEMPLATE PROJECT FOR SIMPLE FIX BOARD AND DAISY SEED ////
 
-///////////////////////////////////////////////////////////////
-///////////////////// LIBRARIES & HARDWARE ////////////////////
+////////////////////////////////////////////////////////////
+//////////////////////// TESTING ///////////////////////////
+// To test knobs uncomment:
+// #define TEST_KNOBS
+
 #include "simple-fix-daisy.h"
 
-///////////////////////////////////////////////////////////////
-/////////////////////////// PINS //////////////////////////////
-// In order to get automatic suggestions of the pins compile 
-// the project once after opening please.
-// To grab a pin capable of ADC, type A( and  press Ctr + Space 
-// to bring the list of ADC capable pins.   
-// To grab a digital pin type D( and press Ctr + Space to bring 
-// the list of digital pins.
-
-auto analog_pin = A(S30);
-auto gpio_pin = D(S31);
+////////////////////////////////////////////////////////////
+///////////////////// KNOBS & SWITCHES /////////////////////
+static const int knob_a = A(S30);
+static const int knob_b = A(S31);
+static const int knob_c = A(S32);
+static const int knob_d = A(S33);
 
 ///////////////////////////////////////////////////////////////
-///////////////////// AUDIO CALLBACK (PATCH) //////////////////
+///////////////////// AUDIO CALLBACK //////////////////////////
 void AudioCallback(float **in, float **out, size_t size) {
   for (size_t i = 0; i < size; i++) {
     out[0][i] = in[0][i];
@@ -33,12 +31,44 @@ void setup() {
   DAISY.init(DAISY_SEED, AUDIO_SR_48K);
   auto sample_rate = DAISY.get_samplerate();
 
-  // initialize modules here
+  // Testing code. Can be removed /////////////////////////////
+  #if defined(TEST_KNOBS)
+  Serial.begin(9600);
+  #endif
+  /////////////////////////////////////////////////////////////
+
+  // initialize other modules here
 
   // BEGIN CALLBACK
   DAISY.begin(AudioCallback);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Process knob values 
+  // auto knob_a_value = analogRead(knob_a);
+
+  auto d = 4; //ms
+  //Testing code. Can be renmoved //////////////////////////////////////////
+  #ifdef TEST_KNOBS
+  testKnobs();
+  #endif
+  #if defined(TEST_KNOBS)
+  d = 100;
+  #endif
+  /////////////////////////////////////////////////////////////////////////
+  delay(d);
 }
+
+#ifdef TEST_KNOBS
+void testKnobs() {
+  Serial.print("S30: ");
+  Serial.print(analogRead(knob_a));
+  Serial.print(" S31: ");
+  Serial.print(analogRead(knob_b));
+  Serial.print(" S32: ");
+  Serial.print(analogRead(knob_c));
+  Serial.print(" S33: ");
+  Serial.print(analogRead(knob_d));
+  Serial.println("");
+}
+#endif
